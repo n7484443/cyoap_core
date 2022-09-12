03553e58981f8f122b34ff3d88a42e516819fffb
n7484443 <n7484443@naver.com>
2022-09-12 19:29:30 +0900

    Initial commit

---
+++ bin/cyoap_core.dart
@@ -0,0 +1,5 @@
+import 'package:cyoap_core/cyoap_core.dart' as cyoap_core;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cyoap_core.calculate()}!');
+}
---
+++ test/cyoap_core_test.dart
@@ -0,0 +1,8 @@
+import 'package:cyoap_core/cyoap_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
