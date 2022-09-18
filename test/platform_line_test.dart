e6137059879402547dfa8cf547339e9016c6cec1
n7484443 <n7484443@naver.com>
2022-09-18 23:59:43 +0900

    test case 추가

--- test/platform_line_test.dart
+++ test/platform_line_test.dart
@@ -1,5 +1,6 @@
 import 'package:cyoap_core/choiceNode/choice_line.dart';
 import 'package:cyoap_core/choiceNode/choice_node.dart';
+import 'package:cyoap_core/choiceNode/selectable_status.dart';
 import 'package:cyoap_core/playable_platform.dart';
 import 'package:test/test.dart';
 
@@ -23,16 +24,19 @@ void main() {
 
     expect(choiceNode0.select, 0);
     expect(choiceNode1.select, 0);
+    expect(lineSetting.selectableStatus, SelectableStatus.open);
     choiceNode0.selectNode(0);
     platform.updateStatusAll();
 
     expect(choiceNode0.select, 1);
     expect(choiceNode1.select, 0);
+    expect(lineSetting.selectableStatus, SelectableStatus.open);
     choiceNode1.selectNode(0);
     platform.updateStatusAll();
 
     expect(choiceNode0.select, 1);
     expect(choiceNode1.select, 0);
+    expect(lineSetting.selectableStatus, SelectableStatus.open);
     choiceNode0.selectNode(0);
     platform.updateStatusAll();
     choiceNode1.selectNode(0);
@@ -40,15 +44,49 @@ void main() {
 
     expect(choiceNode0.select, 0);
     expect(choiceNode1.select, 1);
+    expect(lineSetting.selectableStatus, SelectableStatus.open);
     choiceNode0.selectNode(0);
     platform.updateStatusAll();
 
     expect(choiceNode0.select, 0);
     expect(choiceNode1.select, 1);
+    expect(lineSetting.selectableStatus, SelectableStatus.open);
     choiceNode1.selectNode(0);
     platform.updateStatusAll();
 
     expect(choiceNode0.select, 0);
     expect(choiceNode1.select, 0);
+    expect(lineSetting.selectableStatus, SelectableStatus.open);
+  });
+
+  test('lineHideTest', () {
+    var platform = PlayablePlatform();
+    platform.lineSettings.add(LineSetting(0));
+    var lineSetting = LineSetting(0)..recursiveStatus.conditionVisibleString = "testNode0";
+    lineSetting.generateParser();
+    platform.lineSettings.add(lineSetting);
+    var choiceNode0 = ChoiceNode.empty()..title = "testNode0";
+    var choiceNode1 = ChoiceNode.empty()..title = "testNode1";
+    platform.lineSettings[0].addChildren(choiceNode0);
+    platform.lineSettings[1].addChildren(choiceNode1);
+    platform.updateStatusAll();
+
+    expect(choiceNode1.select, 0);
+    expect(lineSetting.selectableStatus, SelectableStatus.hide);
+    choiceNode0.selectNode(0);
+    platform.updateStatusAll();
+
+    expect(choiceNode1.select, 0);
+    expect(lineSetting.selectableStatus, SelectableStatus.open);
+    choiceNode1.selectNode(0);
+    platform.updateStatusAll();
+
+    expect(choiceNode1.select, 1);
+    expect(lineSetting.selectableStatus, SelectableStatus.open);
+    choiceNode0.selectNode(0);
+    platform.updateStatusAll();
+
+    expect(choiceNode1.select, 1);
+    expect(lineSetting.selectableStatus, SelectableStatus.hide);
   });
 }
