bcbb2c725776c53cf2ec2462ce1f844d89f741ef
n7484443 <n7484443@naver.com>
2022-12-21 13:55:17 +0900

    test 추가

--- test/platform/single_test.dart
+++ test/platform/single_test.dart
@@ -2,13 +2,16 @@ import 'dart:math';
 
 import 'package:cyoap_core/choiceNode/choice_line.dart';
 import 'package:cyoap_core/choiceNode/choice_node.dart';
+import 'package:cyoap_core/grammar/value_type.dart';
 import 'package:cyoap_core/playable_platform.dart';
 import 'package:cyoap_core/variable_db.dart';
 import 'package:test/test.dart';
 
+import '../analyzer/analyzer_tool.dart';
+
 void main() {
-  var ins = VariableDataBase();
   test('defaultModeTest', () {
+    var ins = VariableDataBase();
     var platform = PlayablePlatform();
     platform.lineSettings.add(ChoiceLine(0));
     var choiceNode = ChoiceNode.empty()..title = "testNode";
@@ -25,6 +28,7 @@ void main() {
   });
 
   test('defaultModeTest_nonSelectable', () {
+    var ins = VariableDataBase();
     var platform = PlayablePlatform();
     platform.lineSettings.add(ChoiceLine(0));
     var choiceNode = ChoiceNode.empty()..title = "testNode";
@@ -39,7 +43,39 @@ void main() {
     expect(ins.getValueType("testNode")?.dataUnzip, false);
   });
 
+  test('unSelectableModeTest', () {
+    var ins = VariableDataBase();
+    var platform = PlayablePlatform();
+    platform.lineSettings.add(ChoiceLine(0));
+    var choiceNode = ChoiceNode.empty()..title = "testNode";
+    choiceNode.choiceNodeMode = ChoiceNodeMode.unSelectableMode;
+    choiceNode.recursiveStatus.conditionVisibleString = "testInput";
+    choiceNode.recursiveStatus.executeCodeString = "point += 1";
+    var choiceNode2 = ChoiceNode.empty()..title = "testNode2";
+
+
+    platform.lineSettings[0].addChildren(choiceNode);
+    //platform.lineSettings[0].addChildren(choiceNode2);
+    choiceNode.generateParser();
+
+    platform.globalSetting['testInput'] = ValueTypeWrapper(ValueType.bool(false));
+    platform.globalSetting['point'] = ValueTypeWrapper(ValueType.int(0));
+    platform.updateStatusAll();
+    expect(ins.getValueType("testNode")?.dataUnzip, false);
+    expect(ins.getValueType("point")?.dataUnzip, 0);
+
+    platform.globalSetting['testInput'] = ValueTypeWrapper(ValueType.bool(true));
+    platform.globalSetting['point'] = ValueTypeWrapper(ValueType.int(0));
+    platform.updateStatusAll();
+    expect(ins.getValueType("testNode")?.dataUnzip, true);
+    expect(ins.getValueType("point")?.dataUnzip, 1);
+
+
+    //expect(ins.getValueType("testNode2")?.dataUnzip, true);
+  });
+
   test('randomModeTest', () {
+    var ins = VariableDataBase();
     var seed = 10;
     var platform = PlayablePlatform();
     platform.lineSettings.add(ChoiceLine(0));
@@ -66,6 +102,7 @@ void main() {
   });
 
   test('multiSelectTest', () {
+    var ins = VariableDataBase();
     var platform = PlayablePlatform();
     platform.lineSettings.add(ChoiceLine(0));
     var choiceNode = ChoiceNode.empty()
@@ -105,6 +142,7 @@ void main() {
   });
 
   test('defaultModeTest', () {
+    var ins = VariableDataBase();
     var platform = PlayablePlatform();
     platform.lineSettings.add(ChoiceLine(0));
     var choiceNode = ChoiceNode.empty()..title = "testNode"..choiceNodeMode=ChoiceNodeMode.unSelectableMode;
