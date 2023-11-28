895470d2454133809bbb304d1fdb29d44015257a
n7484443 <n7484443@naver.com>
2023-11-28 09:50:46 +0900

    Update i18n.dart

--- lib/i18n.dart
+++ lib/i18n.dart
@@ -8,10 +8,16 @@ extension Localization on String {
       "fill": "fill",
       "pattern": "pattern",
       "stretch": "stretch",
+
       "none": "none",
       "solid": "solid",
       "dotted": "dotted",
       "dashed": "dashed",
+      "gradient": "gradient",
+
+      "linear": "linear",
+      "radial": "radial",
+      "sweep": "sweep",
     },
     "ko": {
       "choice": "선택지",
@@ -19,10 +25,16 @@ extension Localization on String {
       "fill": "채움",
       "pattern": "패턴",
       "stretch": "늘리기",
+
       "none": "없음",
       "solid": "실선",
       "dotted": "점선",
       "dashed": "파선",
+      "gradient": "그라데이션",
+
+      "linear": "선형",
+      "radial": "방사형",
+      "sweep": "극좌표형",
     },
   };
 
