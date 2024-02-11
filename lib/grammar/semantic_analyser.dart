35e1867e8b4b82aa027af7e13dc3b251c57d1566
n7484443 <n7484443@naver.com>
2024-02-11 23:58:15 +0900

    add compile error

--- lib/grammar/semantic_analyser.dart
+++ lib/grammar/semantic_analyser.dart
@@ -7,18 +7,17 @@ class SemanticAnalyser {
   static const int functionEndSign = -2;
 
   AST abstractSyntaxTreeCompile(dynamic motherToken) {
-    // print(motherToken);
     AST ast = AST(motherToken);
-    // print(jsonEncode(ast.toJson()));
     return ast;
   }
 
   AST? analyseLines(Result<dynamic> analysedData, {optimize = true}) {
-    if (analysedData is Failure) return null;
+    if (analysedData is Failure) throw CompileError();
     var ast = abstractSyntaxTreeCompile(analysedData.value);
-    // if (optimize) {
-    //   return optimizeTree(ast);
-    // }
     return ast;
   }
+}
+
+class CompileError implements Exception {
+  final String message = "Compile Error";
 }
